0e9da90e3e2bcb2ec831734e63aa80b5c221d8a2
patipan-patisampita <patipan.patisampita@gmail.com>
2022-10-20 17:15:20 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'iPhone'
+const app = Vue.createApp({
+    data() {
+        return {
+            product:"iPhone 14",
+            image:'./assets/images/iphone-12-red.png',
+            description:'Apple A14 Bionic',
+            url:'https://www.apple.com',
+            inStock:true,
+            inventory:0,
+        }
+    }
+}).mount("#app")
